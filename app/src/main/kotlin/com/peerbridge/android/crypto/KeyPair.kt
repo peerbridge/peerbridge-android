7efcf3bb08087b2eb053f1dda8a5c6b62d6b82c7
Felix <kaestner.fe@gmail.com>
2021-05-16 14:29:21 +0200

    add restore method for key pair

--- app/src/main/kotlin/com/peerbridge/android/crypto/KeyPair.kt
+++ app/src/main/kotlin/com/peerbridge/android/crypto/KeyPair.kt
@@ -5,5 +5,6 @@ import com.peerbridge.secp256k1.Libsecp256k1
 data class KeyPair(val privateKeyHex: String, val publicKeyHex: String) {
     companion object {
         fun create(): KeyPair = Libsecp256k1.createKeyPairHex().let { KeyPair(it.first, it.second) }
+        fun restore(privateKeyHex: String): KeyPair = KeyPair(privateKeyHex, Libsecp256k1.computePublicKeyHex(privateKeyHex)!!)
     }
 }
