2b87c6644637bbb5c17416d5d6ea81277a516c7f
Felix <kaestner.fe@gmail.com>
2021-05-17 14:11:22 +0200

    delete unused Conversation model

--- app/src/main/kotlin/com/peerbridge/android/model/Conversation.kt
+++
@@ -1,9 +0,0 @@
-package com.peerbridge.android.model
-
-import androidx.compose.runtime.Immutable
-import com.peerbridge.android.crypto.PublicKey
-import kotlinx.serialization.Serializable
-
-@Immutable
-@Serializable
-data class Conversation(val partnerPublicKey: PublicKey, val lastTransaction: Transaction?)
