71a6830e50ad467288b3fbb1824034650b4d895d
Felix <kaestner.fe@gmail.com>
2021-05-17 14:13:43 +0200

    define App Database using Room

---
+++ app/src/main/kotlin/com/peerbridge/android/model/Database.kt
@@ -0,0 +1,9 @@
+package com.peerbridge.android.model
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Transaction::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun transactionDao(): TransactionDao
+}
