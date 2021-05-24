40e289cfb40968271e271aba4b26b9c85ce621ec
Felix <kaestner.fe@gmail.com>
2021-05-24 19:22:51 +0200

    remove unused dependencies

--- secp256k1/build.gradle.kts
+++ secp256k1/build.gradle.kts
@@ -61,13 +61,8 @@ afterEvaluate {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.3.0")
-
     // JUnit - https://github.com/junit-team/junit4
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
