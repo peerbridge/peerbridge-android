89897a857b45174f4d2edddc1a3e6724269d8660
Felix <kaestner.fe@gmail.com>
2021-05-27 15:27:51 +0200

    add database singleton

--- app/src/main/kotlin/com/peerbridge/android/model/Database.kt
+++ app/src/main/kotlin/com/peerbridge/android/model/Database.kt
@@ -1,9 +1,21 @@
 package com.peerbridge.android.model
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Transaction::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "peerbridge").build()
+            }
+        }
+    }
 }
